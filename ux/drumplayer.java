45aa2b952b1e3ae2bfe482dfe595123ac2ab7b8a
randallwvr90 <randy.weaver@flocksafety.com>
2021-03-06 11:26:26 -0500

    Initial commit for drumplayer and updated musicplayer
    I called the thing that would play the drums musicplayer.java but realized that musicplayer should play music files. So I created drumplayer and changed musicplayer accordingly. This is also a chance to try a multi-line commit message from the shell.

---
+++ ux/drumplayer.java
@@ -0,0 +1,13 @@
+/*
+*************************************************************************************************************************
+**** This Java class will handle playing the drums. ****
+
+	What kind of methods does it need?
+	- play_drums
+		- it could have some kind of global variable that will increase or decrease the volume and/or speed. 
+		  Maybe one for each? volume and speed?
+		- Will it play drums for a certain amount of time? Will it play drums until it gets some other input?
+	- change_volume
+	- change_speed
+*************************************************************************************************************************
+*/
--- ux/musicplayer.java
+++ ux/musicplayer.java
@@ -1,13 +1,9 @@
 /*
 *************************************************************************************************************************
-**** This Java class will handle playing the drums. ****
+**** This Java class will handle playing music files. ****
 
 	What kind of methods does it need?
-	- play_drums
-		- it could have some kind of global variable that will increase or decrease the volume and/or speed. 
-		  Maybe one for each? volumen and speed?
-		- Will it play drums for a certain amount of time? Will it play drums until it gets some other input?
-	- change_volume
-	- change_speed
+	- play_music_file
+	- I don't know what else. How will it change based on user input?
 *************************************************************************************************************************
 */
